d0bf9123115921db55386ad4a6d47a73d2837cd4
gilian <gilian@mailfence.com>
2020-11-17 15:07:31 +0100

    add api request

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,38 @@
-import './App.css';
+import { useState, useEffect } from 'react'
 
+import './App.css';
 import Card from './component/card'
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [isLoad, setIsLoaded] = useState(false)
+  const [items, setItems] = useState([])
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/3/movie/550?api_key=549fec9423b36940a428a7f91f7a913f")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+
   return (
     <div className="App">
-        <h1>Movies List (2 selected)</h1>
-        <Card />
+        <h1>Movies List ({count} selected)</h1>
+        <div className="movies-container">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
     </div>
   );
 }
--- my-app/src/component/card.js
+++ my-app/src/component/card.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import './card.css'
 
-export default function Card () {
+export default function Card() {
   return (
-    <div className="card-container">
+    <div className="card-container" >
       <p> 200000</p>
       <p className="title"> title</p>
     </div>
